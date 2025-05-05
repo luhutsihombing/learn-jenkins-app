105615d6ff3536fca2de9cfde1dbf456a7d73a38
luhut sihombing <luhut.paskalis@gmail.com>
2025-05-05 18:23:58 +0000

    small changes in App.js file

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy
+          Learn Jenkins + React JS
         </a>
       </header>
       <p>
-          Application version: 1
+           version: 1
       </p>
     </div>
   );
